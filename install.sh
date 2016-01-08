3f5a5c874991a42e51f3650794641c0626f7e4eb
4679 <btchina@live.com>
2016-01-08 13:27:36 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/4679/vimrc
+mv ~/.vim ~/.vim.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv vimrc/.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
